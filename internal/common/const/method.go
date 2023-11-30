713e6744b8021b380db7ce196000ed807f9d1cf0
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-11-30 17:57:47 -0300

    ✬feat: method and payment const

---
+++ internal/common/const/method.go
@@ -0,0 +1,5 @@
+package consts
+
+func Method() [4]string {
+    return [4]string{"cash", "credit", "debit", "transfer"}
+}
---
+++ internal/common/const/payment.go
@@ -0,0 +1,5 @@
+package consts
+
+func Payment() [4]string {
+    return [4]string{"payment", "deposit", "withdrawal", "transfer"}
+}
