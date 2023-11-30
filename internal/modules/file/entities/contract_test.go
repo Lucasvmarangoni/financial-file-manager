61ddbf30aa92a4edf42bba70e53858c70147ae1c
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-11-30 03:42:34 -0300

    ✬test(unit):  entity contract

---
+++ internal/modules/file/entities/contract_test.go
@@ -0,0 +1,54 @@
+package entities_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/Lucasvmarangoni/financial-file-manager/internal/modules/file/entities"
+)
+
+func TestContractValidate(t *testing.T) {
+	t.Run("should return error when title is empty", func(t *testing.T) {
+		contract := entities.Contract{
+			File: entities.File{
+				ID:        "123e4567-e89b-12d3-a456-426614174000",
+				Type:      "contract",
+				CreatedAt: time.Now(),
+				Customer:  "test-customer",
+			},
+			Title:   "",
+			Parties: []string{"Party 1", "Party 2"},
+			Object:  "Test Object",
+		}
+
+		err := contract.Validate()
+		assert.Error(t, err)
+		assert.Equal(t, "Need a title", err.Error())
+	})
+
+	// Add more tests for other validation cases...
+}
+
+func TestNewContract(t *testing.T) {
+	file := entities.File{
+		ID:        "123e4567-e89b-12d3-a456-426614174000",
+		Type:      "contract",
+		CreatedAt: time.Now(),
+		Customer:  "test-customer",
+	}
+	title := "Test Title"
+	parties := []string{"Party 1", "Party 2"}
+	object := "Test Object"
+	extract := []string{"Extract 1", "Extract 2"}
+	invoice := []string{"Invoice 1", "Invoice 2"}
+
+	contract := entities.NewContract(file, title, parties, object, extract, invoice)
+
+	assert.Equal(t, file, contract.File)
+	assert.Equal(t, title, contract.Title)
+	assert.Equal(t, parties, contract.Parties)
+	assert.Equal(t, object, contract.Object)
+	assert.Equal(t, extract, contract.Extract)
+	assert.Equal(t, invoice, contract.Invoice)
+}
