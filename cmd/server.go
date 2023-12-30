330beccaa5bedb23e49a028e5c3dcc728888b352
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2023-12-30 16:07:44 -0300

    ✬feat(main): add server

---
+++ cmd/server.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Lucasvmarangoni/financial-file-manager/config"
+	"github.com/Lucasvmarangoni/financial-file-manager/internal/common/queue"
+	"github.com/Lucasvmarangoni/financial-file-manager/internal/infra/database"
+	"github.com/Lucasvmarangoni/financial-file-manager/internal/modules/user/http/handlers"
+	"github.com/Lucasvmarangoni/financial-file-manager/internal/modules/user/http/routers"
+	"github.com/Lucasvmarangoni/financial-file-manager/internal/modules/user/infra/repositories"
+	"github.com/Lucasvmarangoni/financial-file-manager/internal/rpc"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
+	"github.com/jackc/pgx/v5"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/pkgerrors"
+	"github.com/streadway/amqp"
+)
+
+var db database.Config
+
+func init() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+
+	db.DbName = config.GetEnv("database.name").(string)
+	db.Port = config.GetEnv("database.port").(string)
+	db.User = config.GetEnv("database.user").(string)
+	db.Password = config.GetEnv("database.password").(string)
+	db.SSLMode = config.GetEnv("database.ssl_mode").(string)
+}
+
+func main() {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	tx, err := Database(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rpc.Connect()
+
+	messageChannel := make(chan amqp.Delivery)
+
+	rabbitMQ := queue.NewRabbitMQ()
+	ch := rabbitMQ.Connect()
+	defer ch.Close()
+	rabbitMQ.Consume(messageChannel)
+
+	r := chi.NewRouter()
+	Web(r, tx)
+
+	http.ListenAndServe(":8000", r)
+}
+
+func Database(ctx context.Context) (pgx.Tx, error) {
+
+	dbConnection, err := db.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer dbConnection.Close(ctx)
+
+	tx, err := dbConnection.Begin(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback(ctx)
+
+	return tx, nil
+}
+
+func Web(r *chi.Mux, tx pgx.Tx) {
+	tokenAuth := config.GetEnv("jwt.tokenAuth").(*jwtauth.JWTAuth)
+
+	userRouter := routers.NewUserRouter(tx, r)
+
+	r.Use(middleware.Logger)
+	userRouter.InitializeUserRoutes()	
+	
+	r.Use(jwtauth.Verifier(tokenAuth))
+	r.Use(jwtauth.Authenticator)
+	userRouter.UserRoutes()
+}
