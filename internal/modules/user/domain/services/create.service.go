f27d35f3522730e3d6ec478d15639625b0e0e33b
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2024-01-05 06:00:15 -0300

    ✬ refactor: changed log file

--- internal/modules/user/domain/services/create.service.go
+++ internal/modules/user/domain/services/create.service.go
@@ -4,18 +4,19 @@ import (
 	"context"
 
 	"github.com/Lucasvmarangoni/financial-file-manager/internal/modules/user/domain/entities"
-	pkg_entities "github.com/Lucasvmarangoni/financial-file-manager/pkg/entities"
 	"github.com/Lucasvmarangoni/financial-file-manager/pkg/errors"
+	"github.com/rs/zerolog/log"
 )
 
-func (u *UserService) Create(name, lastName, cpf, email, password string, admin bool) (pkg_entities.ID, error) {
+func (u *UserService) Create(name, lastName, cpf, email, password string, admin bool) error {
 	newUser, err := entities.NewUser(name, lastName, cpf, email, password, admin)
 	if err != nil {
-		return pkg_entities.Nil(), errors.NewError(err, "entities.NewUser")
+		return errors.NewError(err, "entities.NewUser")
 	}
 	newUser, err = u.Repository.Insert(newUser, context.Background())
 	if err != nil {
-		return pkg_entities.Nil(), errors.NewError(err, "Repository.Insert")
+		return errors.NewError(err, "Repository.Insert")
 	}
-	return newUser.ID, nil
+	log.Info().Str("context", "UserHandler").Msgf("User created successfully (%s)", newUser.ID)
+	return nil
 }
