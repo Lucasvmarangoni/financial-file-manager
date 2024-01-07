b5282d4906494ba494e9ad804d7fae68de697ed2
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2024-01-07 15:50:51 -0300

    ✬ feat: add validation

--- internal/modules/user/http/dto/user.dto.go
+++ internal/modules/user/http/dto/user.dto.go
@@ -1,20 +1,26 @@
 package dto
 
+import "github.com/asaskevich/govalidator"
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
 type UserInput struct {
-	Name     string `json:"name"`
-	LastName string `json:"last_name"`
-	CPF      string `json:"cpf"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Admin    bool   `json:"admin"`
+	Name     string `json:"name" valid:"required,alpha,length(3|10)"`
+	LastName string `json:"last_name" valid:"required,alpha,length(3|50)"`
+	CPF      string `json:"cpf" valid:"required,matches(^[0-9]{3}\\.[0-9]{3}\\.[0-9]{3}-[0-9]{2}$)"`
+	Email    string `json:"email" valid:"email"`
+	Password string `json:"password" valid:"required"`
+	Admin    bool   `json:"admin" valid:"-"`
 }
 
 type AuthenticationInput struct {
-	CPF      string `json:"cpf"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	CPF      string `json:"cpf" valid:"required,matches(^[0-9]{3}\\.[0-9]{3}\\.[0-9]{3}-[0-9]{2}$)"`
+	Email    string `json:"email" valid:"required,email"`
+	Password string `json:"password" valid:"required"`
 }
 
 type GetJWTOutput struct {
-	AccessToken string `json:"access_token"`
+	AccessToken string `json:"access_token" valid:"-"`
 }
