f0b26553451c5c01f5d3db22d8585921a580cdba
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2024-01-15 18:08:12 -0300

    ✬ feat: add rabbitmq and use in the user create

---
+++ internal/modules/user/management/management.go
@@ -0,0 +1,47 @@
+package management
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/Lucasvmarangoni/financial-file-manager/config"
+	"github.com/Lucasvmarangoni/financial-file-manager/internal/modules/user/domain/entities"
+	"github.com/Lucasvmarangoni/financial-file-manager/internal/modules/user/infra/repositories"
+	"github.com/Lucasvmarangoni/financial-file-manager/pkg/errors"
+	"github.com/Lucasvmarangoni/financial-file-manager/pkg/queue"
+	"github.com/rs/zerolog/log"
+	"github.com/streadway/amqp"
+)
+
+type UserManagement struct {
+	Repository *repositories.UserRepositoryDb
+	RabbitMQ   *queue.RabbitMQ
+}
+
+func NewManagement(repository *repositories.UserRepositoryDb, rabbitMQ *queue.RabbitMQ) *UserManagement {
+	return &UserManagement{
+		Repository: repository,
+		RabbitMQ:   rabbitMQ,
+	}
+}
+
+func (m *UserManagement) CreateManagement(messageChannel chan amqp.Delivery) error {
+
+	m.RabbitMQ.Consume(messageChannel, config.GetEnv("rabbitMQ_routingkey_userCreate").(string))
+	
+	for message := range messageChannel {	
+		var user entities.User
+		err := json.Unmarshal(message.Body, &user)
+		if err != nil {
+			return errors.NewError(err, "json.Unmarshal")
+		}
+
+		newUser, err := m.Repository.Insert(&user, context.Background())
+		if err != nil {
+			return errors.NewError(err, "Repository.Insert")
+		}
+		log.Info().Str("context", "UserHandler").Msgf("User created successfully (%s)", newUser.ID)
+	}
+	log.Print("CreateManagement finished")
+	return nil
+}
