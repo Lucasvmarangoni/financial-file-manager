2066a66bf39cda3ca5ff466f621d848734fb4f6e
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2024-01-17 17:35:28 -0300

    ✬ test(unit):  (user) authzadmin.service

---
+++ internal/modules/user/domain/services/authz-admin.service_test.go
@@ -0,0 +1,44 @@
+package services_test
+
+import (
+	"testing"
+
+	"github.com/Lucasvmarangoni/financial-file-manager/internal/modules/user/domain/entities"
+	pkg_entities "github.com/Lucasvmarangoni/financial-file-manager/pkg/entities"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+func TestUserService_AuthzAdmin(t *testing.T) {
+	userService, mockRepo, _ := prepare(t)
+	adminId, _ := pkg_entities.ParseID("52c599f3-af83-4fd9-bfd6-e532918f7b23")
+
+	admin := &entities.User{
+		ID:        adminId,
+		Name:      "John",
+		LastName:  "Doe",
+		CPF:       "123.356.239-00",
+		Email:     "john.do2e@example.com",
+		Password:  password,
+		Admin:     true,
+		CreatedAt: createdAt,
+		UpdatedAt: nil,
+	}
+
+	t.Run("AuthzAdmin", func(t *testing.T) {
+		mockRepo.EXPECT().
+			FindById(gomock.Eq(adminId), gomock.Any()).
+			Return(admin, nil).Times(1)
+
+		mockRepo.EXPECT().
+			FindById(gomock.Eq(id), gomock.Any()).
+			Return(user, nil).Times(1)
+
+		mockRepo.EXPECT().
+			ToggleAdmin(gomock.Eq(id.String()), gomock.Any()).
+			Return(nil).Times(1)
+
+		err := userService.AuthzAdmin(adminId.String(), id.String())
+
+		assert.Nil(t, err)
+	})
+}
