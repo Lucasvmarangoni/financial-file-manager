4c8153e8fb8a784a6c468b1a4c9d38978bfd92df
Lucas V Marangoni - Ldragk <lucasvm.ti@gmail.com>
2024-02-09 20:57:34 -0300

    ✬ feat: remove 'logella' error to use go default error

--- pkg/validate/password.validade.go
+++ pkg/validate/password.validade.go
@@ -1,30 +1,30 @@
 package validate
 
 import (
-	go_err "errors"
-	errors "github.com/Lucasvmarangoni/logella/err"
+	"errors"
 	"regexp"
 )
 
+// Using the golang standard error type because it will be sent in the response
 func ValidatePassword(password string) error {
 	if len(password) < 10 {
-		return errors.ErrCtx(go_err.New("password must be at least 10 characters long"), "validatePassword")
+		return errors.New("password must be at least 10 characters long")
 	}
 
 	if !regexp.MustCompile(`[a-z]`).MatchString(password) {
-		return errors.ErrCtx(go_err.New("password must contain at least one lowercase letter"), "validatePassword")
+		return errors.New("password must contain at least one lowercase letter")
 	}
 
 	if !regexp.MustCompile(`[A-Z]`).MatchString(password) {
-		return errors.ErrCtx(go_err.New("password must contain at least one uppercase letter"), "validatePassword")
+		return errors.New("password must contain at least one uppercase letter")
 	}
 
 	if !regexp.MustCompile(`[0-9]`).MatchString(password) {
-		return errors.ErrCtx(go_err.New("password must contain at least one digit"), "validatePassword")
+		return errors.New("password must contain at least one digit")
 	}
 
 	if !regexp.MustCompile(`[!@#\$%\^&\*]`).MatchString(password) {
-		return errors.ErrCtx(go_err.New("password must contain at least one special character"), "validatePassword")
+		return errors.New("password must contain at least one special character")
 	}
 	return nil
 }
