1f6d83859ae7fd9b0053eca8c4a9c16af5631463
Lucas V. Marangoni <101952043+Lucasvmarangoni@users.noreply.github.com>
2025-01-31 08:33:02 -0300

    Update id.go uuidv7

--- pkg/entities/id.go
+++ pkg/entities/id.go
@@ -1,11 +1,18 @@
 package entities
 
-import "github.com/google/uuid"
+import (
+	"github.com/Lucasvmarangoni/logella/err"
+	"github.com/google/uuid"
+)
 
 type ID = uuid.UUID
 
 func NewID() ID {
-	return ID(uuid.New())
+	uuidV7, err := uuid.NewV7()
+	if err != nil {
+		errors.errCtx(err, "uuid.NewV7()")
+	}
+	return uuidV7
 }
 
 func ParseID(s string) (ID, error) {
